2eae08188d43a741e1fb960ec88a8aae2ed6bb09
mizuhoiimori <tanaka.mizuho.com@gmail.com>
2021-10-24 07:13:02 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :users, only:[:show,:index,:edit,:update]
+  resources :books do
+  resource :favorites, only:[:create, :destroy]
+  end
 end
