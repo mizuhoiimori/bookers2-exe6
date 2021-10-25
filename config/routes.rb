3d4a7b5314b97140d6e6eccd6ddb410626e17564
mizuhoiimori <tanaka.mizuho.com@gmail.com>
2021-10-25 08:07:34 +0000

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to:'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users, only:[:show,:index,:edit,:update]
-  resources :books do
-  resource :favorites, only:[:create, :destroy]
-  resource :book_comments, only:[:create, :destroy]
+
+  resources :users do
+    resource :relationships, only:[:create, :destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
   end
+
+  resources :books do
+    resource :favorites, only:[:create, :destroy]
+    resource :book_comments, only:[:create, :destroy]
+  end
+
 end
